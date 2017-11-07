62fb28ba5bb54d875501e902b05d22fd59b04f98
firdiseptiady <firdis998@gmail.com>
2017-11-07 20:30:04 +0800

    Luas Segitiga

--- soal2_praktikum3.cpp
+++ soal2_praktikum3.cpp
@@ -19,5 +19,13 @@ cin >> bidang;
 		L = p*l;
 		cout << "   Luas persegi panjang : "<< L<<endl;
 	}
-
+	else if (bidang == 'S' || bidang == 's') {
+		cout << "   Mengitung luas segitiga \n";	
+		cout << "   Masukkan alasnya   : ";
+		cin >> a;
+		cout << "   Masukkan tingginya : ";
+		cin >> t;
+		L = 0.5*a*t;
+		cout << "   Luas persegi panjang : "<< L<<endl;
+	}	
 }

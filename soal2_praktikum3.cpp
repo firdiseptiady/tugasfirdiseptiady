70474965a87e610a7e2310ebef5da35ff2df30c9
firdiseptiady <firdis998@gmail.com>
2017-11-07 20:28:21 +0800

    Luas Persegi Panjang

---
+++ soal2_praktikum3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main () {
+char bidang;
+double p,l,a,t,L;
+
+cout << "Pilih Bidang : ";
+cin >> bidang;
+
+	if (bidang == 'P' || bidang == 'p') {	
+		cout << "   Mengitung luas persegi panjang \n";	
+		cout << "   Masukkan panjangnya : ";
+		cin >> p;
+		cout << "   Masukkan lebarnya   : ";
+		cin >> l;
+		L = p*l;
+		cout << "   Luas persegi panjang : "<< L<<endl;
+	}
+
+}
